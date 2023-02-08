933e282ec3577d2c784d09bee9564af382ddadbb
Vitor Trimer <vitortrimer@gmail.com>
2023-02-08 20:01:26 -0300

    Add api call

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/vitortrimer/gopher-rest
 
 go 1.18
 
-require github.com/scraly/learning-go-by-examples/go-rest-api v0.0.0-20221108195858-dd343adb9f57 // indirect
+require (
+    github.com/go-openapi/errors v0.20.0
+    github.com/go-openapi/loads v0.20.2
+    github.com/go-openapi/runtime v0.19.29
+    github.com/go-openapi/spec v0.20.1
+    github.com/go-openapi/strfmt v0.20.0
+    github.com/go-openapi/swag v0.19.13
+    github.com/jessevdk/go-flags v1.5.0
+    github.com/scraly/learning-go-by-examples/go-rest-api v0.0.0-20221108195858-dd343adb9f57 // indirect
+)
+
--- internal/main.go
+++ internal/main.go
@@ -5,13 +5,58 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/go-openapi/loads"
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/scraly/learning-go-by-examples/go-rest-api/pkg/swagger/server/restapi"
+	"github.com/scraly/learning-go-by-examples/go-rest-api/pkg/swagger/server/restapi/operations"
 )
 
 func main() {
+	swaggerSpec, err := loads.Analyzed(restapi.SwaggerJSON, "")
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+	api := operations.NewAPI(swaggerSpec)
+    server := restapi.NewServer(api)
+
+    defer func() {
+        if err := server.Shutdown(); err != nil {
+            log.Fatalln(err)
+        }
+    }()
+
+	
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "Its alive, %q", html.EscapeString(r.URL.Path))
 	})
 
 	log.Println("Listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func Health(operations.CheckHealthParams) middleware.Responder {
+    return operations.NewCheckHealthOK().WithPayload("OK")
+}
+
+func GetHelloUser(user operations.GetHelloUserParams) middleware.Responder {
+    return operations.NewGetHelloUserOK().WithPayload("Hello " + user.User + "!")
+}
+
+func GetGopherByName(gopher operations.GetGopherNameParams) middleware.Responder {
+
+    var URL string
+    if gopher.Name != "" {
+        URL = "https://raw.githubusercontent.com/vitortrimer/gophers/master/" + gopher.Name + ".png"
+    } else {
+        URL = "https://raw.githubusercontent.com/vitortrimer/gophers/master/BELGIUM.png"
+    }
+
+    response, err := http.Get(URL)
+    if err != nil {
+        fmt.Println("error")
+    }
+
+    return operations.NewGetGopherNameOK().WithPayload(response.Body)
 }
